cac5e464d0caa62e18585a171f4b294b7451bc0d
Dalila Spencer <dmspencer@students.unwsp.edu>
2025-11-06 22:42:18 -0600

    Week 10 programs complete

--- Employee.py
+++ Employee.py
@@ -1,3 +1,7 @@
+# Title: Employee Class
+# Author: Dalila Spencer
+# Date: 2025-11-6
+
 # Program #4 Employee Class:
 # Write a class Employee that holds the following data about an employee in attributes: name, ID number, department, and job title.
 
@@ -8,3 +12,22 @@
 # Mark Jones	39119	IT	Programmer
 # Joy Rogers	81774	Manufacturing	Engineer
 # The program should store the data in the three objects, then display the data for each employee on the screen.
+
+
+class Employee:
+    def __init__(self, name, id_num, dep, job):
+        self.name = name
+        self.id_num = id_num
+        self.dep = dep
+        self.job = job
+
+    def get_info(self):
+       return self.name + '\t' + self.id_num + '\t' + self.dep + '\t' + self.job
+
+employee1 = Employee('Susan Meyers', '47899', 'Accounting', 'Vice President')
+employee2 = Employee('Mark Jones', '39119', 'IT', 'Programmer')
+employee3 = Employee('Joy Rogers', '81774', 'Manufacturing', 'Engineer')
+
+for num in [employee1, employee2, employee3]:
+    print(num.get_info())
+
